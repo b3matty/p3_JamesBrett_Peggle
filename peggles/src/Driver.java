24c0392e7b476e3b67dcb86730da6fd8171f8650
dcbang747 <31807533+dcbang747@users.noreply.github.com>
2021-06-14 13:56:57 -0700

    Made the Main Menu jFrame Accessible through Both the credits and the Options JFrame interfaces along with adding the correct information in the credits. Further optimized the main game code to work better on faster machines along with minor bug fixes throughout the code. Added more and unique music to each of the menus.

--- peggles/src/Driver.java
+++ peggles/src/Driver.java
@@ -34,12 +34,16 @@ public class Driver extends JPanel implements MouseMotionListener{
 	// JFrame for menu
     static JFrame menu;
     static JFrame credits;
+    static JFrame options;
+    static Color bg = new Color(255,215,0);
+    static Color bg2 = new Color(211,211,211);
     // JButton
-    static JButton b, b1, b2, b3;
+    static JButton b, b1, b2, b3, b4;
  
     // label to display text
     static JLabel l;
 	static JLabel names;
+	static JLabel games;
    
 	public static void main(String[] args)
 	{
@@ -72,6 +76,8 @@ public class Driver extends JPanel implements MouseMotionListener{
    menu.setLocationRelativeTo(null);
    credits = new JFrame("credits");
    credits.setLocationRelativeTo(null);
+   options = new JFrame("options");
+   options.setLocationRelativeTo(null);
    
    
   JFrame frame = new JFrame();
@@ -81,13 +87,14 @@ public class Driver extends JPanel implements MouseMotionListener{
    // create a label to display text
    l = new JLabel("PEGGLE");
    names = new JLabel("Brett Matthews && James Bennett");
+   games = new JLabel("'Options are optional' - David C.");
    
    // create a new buttons
    b = new JButton("Play");
    b1 = new JButton("Options");
    b2 = new JButton("Credits");
-   
    b3 = new JButton ("Back");
+   b4 = new JButton ("Back");
    // create a panel to add buttons
    
    
@@ -96,6 +103,7 @@ public class Driver extends JPanel implements MouseMotionListener{
    JPanel p = new JPanel();
    JPanel p1 = new JPanel();
    Panel panel = new Panel();
+   JPanel opt = new JPanel();
    //Panel panel = new Panel();
    frame.add(panel);
    
@@ -108,18 +116,22 @@ public class Driver extends JPanel implements MouseMotionListener{
    
    p1.add(names);
    p1.add(b3);
+   opt.add(b4);
+   opt.add(games);
   // Background background = new background; 
  
    
    
    // setbackground of panel
    p.setBackground(Color.CYAN);
-   p1.setBackground(Color.CYAN);
+   p1.setBackground(bg2);
    panel.setBackground(Color.black);
+   opt.setBackground(bg);
    
    // add panel to frame
    menu.add(p);
    credits.add(p1);
+   options.add(opt);
    
    // set the size of frame
    menu.setSize(600, 400);
@@ -164,9 +176,23 @@ public class Driver extends JPanel implements MouseMotionListener{
 	  public void actionPerformed(ActionEvent e)
 	  {
 	    menu.setVisible(false);
-	    JFrame options = new JFrame();
+	    options.setVisible(true);
 	    options.setSize(400,400);
 		options.setVisible(true);
+
+	    //music
+  		muteControl.setValue(true);
+  		Clip clip5 = null;
+  		try {
+  	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("Options.wav").getAbsoluteFile());
+  	        clip5 = AudioSystem.getClip();
+  	        clip5.open(audioInputStream);
+  	        clip5.start();
+  	        clip5.loop(0);
+  	        clip5.flush();
+  	    } catch(Exception ex) {
+  	        System.out.println("Error with playing sound.");
+  	    }
 		
 	  }
 	});
@@ -206,6 +232,21 @@ public class Driver extends JPanel implements MouseMotionListener{
 		
 	  }
 	});
+   b4.addActionListener(new ActionListener()
+	{
+	  public void actionPerformed(ActionEvent e)
+	  {
+	    //display/center the jdialog when the button is pressed
+		  options.setVisible(false);
+		  menu.setVisible(true);
+		  
+		  //music
+		 
+		
+		
+	  }
+	});
+   
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
